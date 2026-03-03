6c1c313b66442435e330d98ff6945de21cd0f7b9
vitor-felipee <vitor07.felipe@gmail.com>
2026-03-02 23:11:38 -0300

    Primeiro Commit: configuracao eslint, babel e webpack

---
+++ js/debounce.js
@@ -0,0 +1,9 @@
+export default function debounce(callback, delay) {
+  let timer;
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
