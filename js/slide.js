2e132ee0a391a2034916396c687f5838e08d5e2c
vitor-felipee <vitor07.felipe@gmail.com>
2026-03-04 23:10:04 -0300

    customizando controles estilizados

--- js/slide.js
+++ js/slide.js
@@ -156,7 +156,7 @@ export class Slide {
   }
 }
 
-export class SlideNav extends Slide {
+export default class SlideNav extends Slide {
   constructor(slide, wrapper) {
     super(slide, wrapper);
     this.bindControlEvent();
